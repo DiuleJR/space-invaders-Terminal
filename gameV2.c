193e71f0710a910ae56cfd9d24d66f782102b35c
DiuleJR <syfras999@gmail.com>
2024-10-05 01:33:42 -0300

    Adicionando uma estrutura pro inimigo, criando uma função para carregar os elementos na tela e testando todas as funções na função principal.

--- gameV2.c
+++ gameV2.c
@@ -23,7 +23,14 @@ typedef struct {
     int pos_y;
 } Projetil_Nave;
 
+typedef struct {
+    int vida;
+    int pos_x;
+    int pos_y;
+} Inimigo;
 
+
+// Inicializando as variáveis de game e jogador
 Game game;
 Nave jogador;
 
@@ -39,6 +46,35 @@ void carregar_config_jogador() {
     jogador.vidas = 3;
     jogador.pos_x = TAMANHO_JANELA - 3;
     jogador.pos_y = TAMANHO_JANELA / 2;
+}
+
+void carregar_tela() {
+    for (int i = 0; i < game.janela_x; i++) {
+        for (int j = 0; j < game.janela_y; j++) {
+            if (i == 0 || j == 0 || i == game.janela_x - 1 || j == game.janela_y - 1) {
+                printf("* ");
+            }
+
+            else {
+                if (jogador.pos_x == i && jogador.pos_y == j) {
+                    printf("^ ");
+                }
+                else {
+                    printf("  ");
+                }
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main () {
+
+    carregar_config_game();
+    carregar_config_jogador();
+    carregar_tela();
+
+    return 0;
 }
 
 // jogar tudo na função principal
