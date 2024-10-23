98079e27ebf66006f8b3024121b69e6505ce6723
Diule Junior <97720544+DiuleJR@users.noreply.github.com>
2024-10-23 19:54:59 -0300

    Update gameV3.c
    modificações que o Denny fez o/

--- gameV3.c
+++ gameV3.c
@@ -10,16 +10,16 @@
 #define ESPACO_VAZIO ' '
 #define ATRASO_TIQUE 150
 
-
-
 // --------- Criar as estrutras -------- //
-typedef struct {
+typedef struct
+{
     int game_over;
     int score;
     int hi_score;
 } Jogo;
 
-typedef struct {
+typedef struct
+{
     int vidas;
     int pos_x;
     int pos_y;
@@ -27,13 +27,15 @@ typedef struct {
     char sprite;
 } Jogador;
 
-typedef struct {
+typedef struct
+{
     int pos_x;
     int pos_y;
     char sprite;
 } Projetil;
 
-typedef struct {
+typedef struct
+{
     int vida;
     int pos_x;
     int pos_y;
@@ -54,19 +56,22 @@ COORD posicaoCaractere = {0, 0};
 SMALL_RECT areaEscritaConsole = {0, 0, LARGURA - 1, ALTURA - 1};
 
 // --------- Criar Funções -------- //
-void carregar_config_jogo() {
+void carregar_config_jogo()
+{
     jogo.game_over = 0;
     jogo.hi_score = 0;
     jogo.score = 0;
 }
 
-void carregar_config_projetil() {
+void carregar_config_projetil()
+{
     projetil.pos_x = 999;
     projetil.pos_y = 999;
     projetil.sprite = '^';
 }
 
-void carregar_config_jogador() {
+void carregar_config_jogador()
+{
     jogador.vidas = 3;
     jogador.disparo = 0;
     jogador.pos_x = LARGURA / 2;
@@ -74,7 +79,8 @@ void carregar_config_jogador() {
     jogador.sprite = 'A';
 }
 
-void carregar_menu_jogo() {
+void carregar_menu_jogo()
+{
     char escolha = ' ';
 
     char menu[11][43] = {
@@ -88,22 +94,26 @@ void carregar_menu_jogo() {
         "|               COMO JOGAR                |",
         "|       *                                 |",
         "|               QUIT                      |",
-        "+ - - - - - - - - - - - - - - - - - - - - +"
-    };
+        "+ - - - - - - - - - - - - - - - - - - - - +"};
 
-    while(1) {
+    while (1)
+    {
         system("CLS");
-        for (int i = 0; i < 11; i++) {
-            for(int j = 0; j < 43; j++) {
+        for (int i = 0; i < 11; i++)
+        {
+            for (int j = 0; j < 43; j++)
+            {
                 printf("%c", menu[i][j]);
             }
             printf("\n");
         }
-        
+
         escolha = getch();
-        
-        if (escolha == 'w' || escolha == 'W') {
-            if (menu[3][13] == '>') {
+
+        if (escolha == 'w' || escolha == 'W')
+        {
+            if (menu[3][13] == '>')
+            {
                 menu[3][13] = ' ';
                 menu[3][14] = ' ';
 
@@ -111,35 +121,38 @@ void carregar_menu_jogo() {
                 menu[9][14] = '>';
             }
 
-            else if (menu[9][13] == '>') {
+            else if (menu[9][13] == '>')
+            {
                 menu[9][13] = ' ';
                 menu[9][14] = ' ';
-                
+
                 menu[7][13] = '>';
                 menu[7][14] = '>';
             }
 
-            else if (menu[7][13] == '>') {
+            else if (menu[7][13] == '>')
+            {
                 menu[7][13] = ' ';
                 menu[7][14] = ' ';
-                
+
                 menu[5][13] = '>';
                 menu[5][14] = '>';
             }
 
-            else if (menu[5][13] == '>') {
+            else if (menu[5][13] == '>')
+            {
                 menu[5][13] = ' ';
                 menu[5][14] = ' ';
-                
+
                 menu[3][13] = '>';
                 menu[3][14] = '>';
             }
-
-            
         }
 
-        else if (escolha == 's' || escolha == 'S') {
-            if (menu[3][13] == '>') {
+        else if (escolha == 's' || escolha == 'S')
+        {
+            if (menu[3][13] == '>')
+            {
                 menu[3][13] = ' ';
                 menu[3][14] = ' ';
 
@@ -147,49 +160,51 @@ void carregar_menu_jogo() {
                 menu[5][14] = '>';
             }
 
-            else if (menu[5][13] == '>') {
+            else if (menu[5][13] == '>')
+            {
                 menu[5][13] = ' ';
                 menu[5][14] = ' ';
-                
+
                 menu[7][13] = '>';
                 menu[7][14] = '>';
             }
 
-            else if (menu[7][13] == '>') {
+            else if (menu[7][13] == '>')
+            {
                 menu[7][13] = ' ';
                 menu[7][14] = ' ';
-                
+
                 menu[9][13] = '>';
                 menu[9][14] = '>';
             }
 
-            else if (menu[9][13] == '>') {
+            else if (menu[9][13] == '>')
+            {
                 menu[9][13] = ' ';
                 menu[9][14] = ' ';
-                
+
                 menu[3][13] = '>';
                 menu[3][14] = '>';
             }
-            
         }
 
-        else if (escolha == 'e' || escolha == 'E') {
-            if (menu[3][13] == '>') {
+        else if (escolha == 'e' || escolha == 'E')
+        {
+            if (menu[3][13] == '>')
+            {
                 system("CLS");
                 break;
             }
 
-            // 
+            //
         }
 
         escolha = ' ';
-        
     }
-    
 }
 
-
-void carregar_config_inimigo() {
+void carregar_config_inimigo()
+{
 
     int filas_inimigos = 3;
 
@@ -198,10 +213,12 @@ void carregar_config_inimigo() {
     // Centraliza os inimigos
     int inimigo_x_inicial = (LARGURA - (TOTAL_INIMIGOS_POR_FILA - 1) * intervalo_inimigos) / 2;
     int inimigo_y_inicial = 1;
-    
+
     int inimigo_contador = 0;
-    for (int i = 0; i < filas_inimigos; i++) {
-        for (int j = 0; j < TOTAL_INIMIGOS_POR_FILA; j++) {
+    for (int i = 0; i < filas_inimigos; i++)
+    {
+        for (int j = 0; j < TOTAL_INIMIGOS_POR_FILA; j++)
+        {
             inimigos[inimigo_contador].vida = 1;
             inimigos[inimigo_contador].pos_x = inimigo_x_inicial + (j * intervalo_inimigos);
             inimigos[inimigo_contador].pos_y = inimigo_y_inicial;
@@ -210,13 +227,11 @@ void carregar_config_inimigo() {
             inimigo_contador++;
         }
         inimigo_y_inicial++;
-        
     }
-
-    
 }
 
-void carregar_game_over() {
+void carregar_game_over()
+{
     char menu_gameOver[12][43] = {
         "                 GAME OVER                 ",
         "+ - - - - - - - - - - - - - - - - - - - - +",
@@ -229,57 +244,67 @@ void carregar_game_over() {
         "|              MENU                       |",
         "|     *                                   |",
         "|              QUIT                       |",
-        "+ - - - - - - - - - - - - - - - - - - - - +"
-    };
+        "+ - - - - - - - - - - - - - - - - - - - - +"};
 
-    while(1) {
-        for (int i = 0; i < 12; i++) {
-            for(int j = 0; j < 43; j++) {
+    while (1)
+    {
+        for (int i = 0; i < 12; i++)
+        {
+            for (int j = 0; j < 43; j++)
+            {
                 printf("%c", menu_gameOver[i][j]);
             }
             printf("\n");
         }
         scanf("%d");
     }
-    
 }
 
-void controlar_jogador() {
+void controlar_jogador()
+{
     char keyPress;
 
     // Verifica se uma tecla foi pressionada
-    if (kbhit()) {
+    if (kbhit())
+    {
         keyPress = getch();
-    } else {
+    }
+    else
+    {
         keyPress = ' ';
     }
 
-    if (keyPress == 'a' || keyPress == 'A') {
-        if (jogador.pos_x > 1) {
+    if (keyPress == 'a' || keyPress == 'A')
+    {
+        if (jogador.pos_x > 1)
+        {
             jogador.pos_x -= 1;
         }
-        
     }
 
-    if (keyPress == 'w' || keyPress == 'W') {
-        if (jogador.disparo == 0) {
+    if (keyPress == 'w' || keyPress == 'W')
+    {
+        if (jogador.disparo == 0)
+        {
             jogador.disparo = 1;
             projetil.pos_x = jogador.pos_x;
             projetil.pos_y = jogador.pos_y - 1;
         }
     }
 
-    if (keyPress == 'd' || keyPress == 'D') {
-        if (jogador.pos_x < LARGURA - 2) {
+    if (keyPress == 'd' || keyPress == 'D')
+    {
+        if (jogador.pos_x < LARGURA - 2)
+        {
             jogador.pos_x += 1;
         }
     }
 
-    //criei para testar a tela de game over, caso o player digitar p ou P ele quita da partida
-    if (keyPress == 'p' || keyPress == 'P') {
+    // criei para testar a tela de game over, caso o player digitar p ou P ele quita da partida
+    if (keyPress == 'p' || keyPress == 'P')
+    {
         jogador.vidas = 0;
     }
-    
 }
 
 int intervalo_de_movimento = 0;
@@ -294,89 +319,86 @@ void desenhar_tela()
         bufferConsole[i].Char.AsciiChar = ESPACO_VAZIO;                                    // Preenche com espaços
         bufferConsole[i].Attributes = FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED; // Define cor padrão
     }
-    
+    if (jogador.disparo == 1)
+    {
+
+        int indice = projetil.pos_y * LARGURA + projetil.pos_x;
+        bufferConsole[indice].Char.AsciiChar = projetil.sprite;
+        projetil.pos_y -= 1;
+
+        if (projetil.pos_y == 0)
+        {
+            jogador.disparo = 0;
+        }
+    }
+
+    // Imprime o jogador na tela
+    int indice = jogador.pos_y * LARGURA + jogador.pos_x;
+    bufferConsole[indice].Char.AsciiChar = jogador.sprite;
+
     int index = 0;
-    for (int i = 0; i < ALTURA; i++) {
-        for (int j = 0; j < LARGURA; j++) {
-            if (jogador.disparo == 1) {
-                if (projetil.pos_x == j && projetil.pos_y == i) {
-                    int indice = projetil.pos_y * LARGURA + projetil.pos_x;
-                    bufferConsole[indice].Char.AsciiChar = projetil.sprite;
-                    projetil.pos_y -= 1;
 
-                    if (projetil.pos_y == 0) {
-                        jogador.disparo = 0;
-                    }
+    for (int index = 0; index < TOTAL_INIMIGOS; index++)
+    {
 
-                }
+        if (inimigos[index].vida != 0)
+        {
+            int indice = inimigos[index].pos_y * LARGURA + inimigos[index].pos_x;
+            bufferConsole[indice].Char.AsciiChar = inimigos[index].sprite;
+        }
+
+        if (intervalo_de_movimento == 5)
+        {
+            if (inimigos[TOTAL_INIMIGOS - 1].pos_x >= LARGURA - 2)
+            {
+                deslocamento_inimigo *= -1;
+                // inimigos[index].pos_y++;
             }
+            // inimigos[index].pos_x += deslocamento_inimigo;
+        }
+    }
 
-            if (inimigos[index].pos_x == j && inimigos[index].pos_y == i) {
-                if (jogador.disparo == 1 & projetil.pos_x == inimigos[index].pos_x && projetil.pos_y == inimigos[index].pos_y && inimigos[index].vida > 0) {
-                    jogador.disparo = 0;
-                    inimigos[index].vida--;
-                }
-                
-                if (inimigos[index].vida == 0) {
-                    inimigos[index].sprite = ' ';
-                }
+    for (int i = 0; i < ALTURA; i++)
+    {
+        for (int j = 0; j < LARGURA; j++)
+        {
 
-                int indice = inimigos[index].pos_y * LARGURA + inimigos[index].pos_x;
-                bufferConsole[indice].Char.AsciiChar = inimigos[index].sprite;
-
-                if (intervalo_de_movimento == 5) {
-                    if (inimigos[TOTAL_INIMIGOS - 1].pos_x >= LARGURA - 2) {
-                        deslocamento_inimigo *= -1;
-                        //inimigos[index].pos_y++;
-                    }
-                    // inimigos[index].pos_x += deslocamento_inimigo;
-                }
-                
-
-                index++;
-
-            }
-
-            if (jogador.pos_x == j && jogador.pos_y == i) {
-                // Imprime o jogador na tela
-                int indice = jogador.pos_y * LARGURA + jogador.pos_x;
-                bufferConsole[indice].Char.AsciiChar = jogador.sprite; 
-
-            }
-
-            else if (i == 0 || j == 0 || i == ALTURA - 1 || j == LARGURA - 1) {
-                if (j == 0 || j % 2 == 0) {
+            if (i == 0 || j == 0 || i == ALTURA - 1 || j == LARGURA - 1)
+            {
+                if (j == 0 || j % 2 == 0)
+                {
                     // Imprime as bordas da tela
                     int indice = i * LARGURA + j;
                     bufferConsole[indice].Char.AsciiChar = '*';
                 }
-        
             }
-
         }
     }
-    if (intervalo_de_movimento == 5) {
+    if (intervalo_de_movimento == 5)
+    {
         intervalo_de_movimento = 0;
     }
-    else {
+    else
+    {
         intervalo_de_movimento++;
-    }   
+    }
     WriteConsoleOutputA(console, bufferConsole, tamanhoBuffer, posicaoCaractere, &areaEscritaConsole);
 }
 
-//função que vai gerar... game over
-void gerar_gameOver() {
-    if(jogador.vidas == 0){
+// função que vai gerar... game over
+void gerar_gameOver()
+{
+    if (jogador.vidas == 0)
+    {
         jogo.game_over = 0;
     }
 }
 
-
-
 // jogar tudo na função principal
 
-int main() {
- 
+int main()
+{
+
     carregar_menu_jogo();
     carregar_config_jogo();
     carregar_config_jogador();
@@ -384,14 +406,16 @@ int main() {
     carregar_config_inimigo();
 
     console = GetStdHandle(STD_OUTPUT_HANDLE);
-    
-    while(1) {
+
+    while (1)
+    {
         desenhar_tela();
         controlar_jogador();
         Sleep(ATRASO_TIQUE);
     }
 
-    if(jogo.game_over == 1){
+    if (jogo.game_over == 1)
+    {
         carregar_game_over;
     }
 
